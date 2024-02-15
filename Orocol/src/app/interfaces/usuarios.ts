e15046352025ea0caff99eee0229606839698763
joshuacruz123 <joshua.cruz@soy.sena.edu.co>
2024-02-15 15:23:51 -0500

    front

--- Orocol/src/app/interfaces/usuarios.ts
+++
@@ -1,9 +0,0 @@
-export interface Persona {
-    id?: number;
-    nombre: string;
-    apellido: string;
-    correo: String;
-    tipoDocumento: string;
-    documento: number;
-    fechaNacimiento: Date;
-}
